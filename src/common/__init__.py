cda42fc43ce809bd998c36c5ba1129192c573ef7
mbegma <begma.mv@igit.spb.ru>
2026-03-13 17:48:32 +0300

    add import query template

--- src/common/__init__.py
+++ src/common/__init__.py
@@ -11,9 +11,13 @@
 from src.common.consts import RET_CODE_OK, RET_CODE_ERROR
 from src.common.consts import SR_WEB_MERCATOR, SR_WGS84, SR_EQUIDISTANT_CONIC
 from src.common.consts import EMPTY_POLYGON, EMPTY_POINT, EMPTY_LINE
+# from src.common.consts import DIVISION_AREA_TBL, DIVISION_BOUNDARY_TBL, DIVISION
+
 from src.common.custom_logger import get_log_filename, create_logger_ext, get_logger_random_name
 from src.common.custom_logger import LOG_HDL_CNSL, LOG_HDL_ROT_FILE, LOG_HDL_FILE, LOG_FMT
+
 from src.common import utilities
+from src.common import query_templates
 
 
 
