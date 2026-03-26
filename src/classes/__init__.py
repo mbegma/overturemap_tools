e81286497ed290a9d7dfcfd08675030e9ff7dd33
mbegma <begma.mv@igit.spb.ru>
2026-03-26 17:08:54 +0300

    add import TableInfo

--- src/classes/__init__.py
+++ src/classes/__init__.py
@@ -11,7 +11,7 @@
 #        - 15.05.2024: start of development
 # -----------------------------------------------------
 from src.classes.error_classes import InputDataError, ProcessDataError
-from src.classes.core import Core
+from src.classes.core import Core, TableInfo
 
 
 
