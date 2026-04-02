d7f9a0b328596fff61cf453a880d9d7692129d16
mbegma <begma.mv@igit.spb.ru>
2026-04-02 17:08:43 +0300

    import EXPORT_FORMAT

--- src/common/__init__.py
+++ src/common/__init__.py
@@ -11,7 +11,7 @@
 from src.common.consts import RET_CODE_OK, RET_CODE_ERROR
 from src.common.consts import SR_WEB_MERCATOR, SR_WGS84, SR_EQUIDISTANT_CONIC
 from src.common.consts import EMPTY_POLYGON, EMPTY_POINT, EMPTY_LINE
-# from src.common.consts import DIVISION_AREA_TBL, DIVISION_BOUNDARY_TBL, DIVISION
+from src.common.consts import EXPORT_FORMAT
 
 from src.common.custom_logger import get_log_filename, create_logger_ext, get_logger_random_name
 from src.common.custom_logger import LOG_HDL_CNSL, LOG_HDL_ROT_FILE, LOG_HDL_FILE, LOG_FMT
