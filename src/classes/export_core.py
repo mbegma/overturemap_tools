fe2d713ff355abe6abf8590823d55545317a3b9a
mbegma <begma.mv@igit.spb.ru>
2026-04-03 18:36:21 +0300

    add Table dict, add _create_output_file_name(), _export_table() and export() functions

--- src/classes/export_core.py
+++ src/classes/export_core.py
@@ -15,7 +15,8 @@ import logging
 from typing import List
 
 from pathlib import Path
-# from datetime import datetime
+
+
 from src.config import config
 from src.common import utilities as u
 import duckdb
@@ -23,6 +24,60 @@ from jinja2 import Template
 from src.common import query_templates
 from src.common import EXPORT_FORMAT
 
+TABLES = {
+    config.TBL_NAME_BASE_LAND: {
+        "template": query_templates.QUERY_EXPORT_BASE_LAND,
+        "geom": ['pnt', 'lin', 'pol']
+    },
+    config.TBL_NAME_BASE_LAND_USE: {
+        "template": query_templates.QUERY_EXPORT_BASE_LAND_USE,
+        "geom": ['pnt', 'lin', 'pol']
+    },
+    config.TBL_NAME_BASE_LAND_COVER: {
+        "template": query_templates.QUERY_EXPORT_BASE_INFRASTRUCTURE,
+        "geom": ['pnt', 'lin', 'pol']
+    },
+    config.TBL_NAME_BASE_INFRASTRUCTURE: {
+        "template": query_templates.QUERY_EXPORT_BASE_INFRASTRUCTURE,
+        "geom": ['pnt', 'lin', 'pol']
+    },
+    config.TBL_NAME_BASE_WATER: {
+        "template": query_templates.QUERY_EXPORT_BASE_WATER,
+        "geom": ['pnt', 'lin', 'pol']
+    },
+    config.TBL_NAME_PLACES_PLACE: {
+        "template": query_templates.QUERY_EXPORT_PLACES_PLACE,
+        "geom": ['pnt']
+    },
+    config.TBL_NAME_BUILDINGS_BUILDING: {
+        "template": query_templates.QUERY_EXPORT_BUILDINGS_BUILDING,
+        "geom": ['pol']
+    },
+    config.TBL_NAME_BUILDINGS_BUILDING_PART: {
+        "template": query_templates.QUERY_EXPORT_BUILDINGS_BUILDING_PART,
+        "geom": ['pol']
+    },
+    config.TBL_NAME_TRANSPORTATION_SEGMENT: {
+        "template": query_templates.QUERY_EXPORT_TRANSPORTATION_SEGMENT,
+        "geom": ['pnt', 'lin', 'pol']
+    },
+    config.TBL_NAME_TRANSPORTATION_CONNECTOR: {
+        "template": query_templates.QUERY_EXPORT_TRANSPORTATION_SEGMENT,
+        "geom": ['pnt', 'lin', 'pol']
+    },
+    config.TBL_NAME_DIVISIONS_AREA: {
+        "template": query_templates.QUERY_EXPORT_DIVISIONS_AREA,
+        "geom": ['pol']
+    },
+    config.TBL_NAME_DIVISIONS_BOUNDARY: {
+        "template": query_templates.QUERY_EXPORT_DIVISIONS_BOUNDARY,
+        "geom": ['lin']
+    },
+    config.TBL_NAME_DIVISIONS_DIVISION: {
+        "template": query_templates.QUERY_EXPORT_DIVISIONS_DIVISION,
+        "geom": ['pnt']
+    },
+}
 
 class ExportCore:
     _ver = "1.0.0"
@@ -32,11 +87,10 @@ class ExportCore:
         self.error = None
 
         self.db_name = kwargs.get("dbname", "")
-        self.output_dir = kwargs.get("output_dir", None)
-        self.tables = kwargs.get("tables", [])
+        self.output_dir: str = kwargs.get("output_dir", "")
+        self.tables_list = kwargs.get("tables", [])
         self.is_wm = kwargs.get("is_wm", False)
         self.format = kwargs.get("format", "GPKG")
-
 
     def get_last_error(self):
         return self.error
@@ -51,20 +105,21 @@ class ExportCore:
     def set_parameters(self, parameters: dict):
         self.log.debug(parameters)
         self.db_name = parameters.get("db_name", "")
-        self.output_dir = parameters.get("output_dir", None)
-        self.tables = parameters.get("tables", [])
+        self.output_dir = parameters.get("output_dir", "")
+        self.tables_list = parameters.get("tables", [])
         self.is_wm = parameters.get("is_wm", False)
         self.format = parameters.get("format", "GPKG")
-        if self.db_name == "":
+
+        if self.db_name != "":
             if Path(self.db_name).suffix == "":
                 self.db_name = f"{self.db_name}.duckdb"
         else:
             self._set_error(f"Database name not specified")
 
-        if len(self.tables) == 0:
-            self.tables = self._get_db_tables_name(db_name=self.db_name)
+        if len(self.tables_list) == 0:
+            self.tables_list = self._get_db_tables_name(db_name=self.db_name)
         else:
-            self.tables = list(self.tables)
+            self.tables_list = list(self.tables_list)
 
     def _get_db_tables_name(self, db_name: str) -> List[str]:
         """
@@ -81,8 +136,62 @@ class ExportCore:
             self._set_error(str(e.args))
             return []
 
+    def _create_output_file_name(self, table_name: str, geometry_type: str) -> str:
+        _extension = '.gpkg' if self.format == 'GPKG' else '.geojson'
+        if geometry_type == 'lin':
+            _postfix = '_lin'
+        elif geometry_type == 'pol':
+            _postfix = '_pol'
+        else:
+            _postfix = '_pnt'
+        return f"{table_name}{_postfix}{_extension}"
 
+    def _export_table(self, table_name: str) -> bool:
+        self.log.debug(f"{u.tab()}export data from <{table_name}> ...")
+        if not table_name in TABLES.keys():
+            self._set_error(f"Table {table_name} not found in processed tables.")
+            return False
+        _table = TABLES[table_name]
+        try:
+            with duckdb.connect(Path(config.DB_DIR) / self.db_name) as con:
+                con.sql("INSTALL SPATIAL;")
+                con.sql("LOAD SPATIAL;")
+                con.sql("INSTALL json;")
+                con.sql("LOAD json;")
+                for geom in _table['geom']:
+                    _file_name = self._create_output_file_name(table_name, geom)
+                    self.log.debug(f"{u.tab(2)}export {geom.upper()} data from <{table_name}> to {_file_name} ...")
+                    _query = Template(_table['template']).render(
+                        is_wm=self.is_wm,
+                        table_name=table_name,
+                        geom=geom,
+                        file_name= Path(self.output_dir) / _file_name,
+                        driver='GPKG' if self.format == 'GPKG' else 'GeoJSON',
+                        layer_name=Path(_file_name).stem,
+                    )
+                    self.log.debug(f"{u.tab(2)}sql query: {_query}")
+                    ret_val = con.sql(_query)
+                    self.log.debug(f"{u.tab(2)}sql result: {ret_val}")
+                    self.log.debug(f"{u.tab(2)}export {geom.upper()} data from <{table_name}> "
+                                   f"to {_file_name} successfully finished")
+                self.log.debug(f"{u.tab()}export table {table_name} successfully finished")
+            return True
+        except Exception as e:
+            self._set_error(str(e.args))
+            return False
 
+    def export(self) -> bool:
+        try:
+            for table_name in self.tables_list:
+                if self._export_table(table_name=table_name):
+                    self._set_info(f"The {table_name} table export was successful.")
+                else:
+                    self._set_error(f"Error exporting table {table_name}. "
+                                    f"Error: {self.get_last_error()}")
+            return True
+        except Exception as e:
+            self._set_error(str(e.args))
+            return False
 
 def main():
     # region local log
@@ -104,17 +213,19 @@ def main():
     params = {
         "db_name": "ru-iva-s",
         "output_dir": r"D:\PyProjects\overturemaps\output\tmp",
-        "tables": ['base_land', 'base_water', 'places_place'],
-        "is_wm": False,
+        "tables": ['base_water'],
+        "is_wm": True,
         "format": EXPORT_FORMAT[0] # GPKG
     }
     try:
         cl = ExportCore(class_logger=log)
 
-        ret = cl._get_db_tables_name(db_name="ru-iva-s.duckdb")
-        print(ret)
+        # ret = cl._get_db_tables_name(db_name="ru-iva-s.duckdb")
+
 
         cl.set_parameters(params)
+        ret = cl.export()
+        print(ret)
     except Exception as e:
         log.error(str(e.args))
 
